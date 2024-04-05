ab949a77470724814e45801d6ed8bcc581399326
Raiyan109 <kraiyan109@gmail.com>
2024-04-05 13:03:40 +0600

    initialize

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00C397',
+        secondary: '#BFF4BE'
+      }
+    },
   },
   plugins: [],
 }
